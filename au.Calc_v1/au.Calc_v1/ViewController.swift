08bf53352d4d9385c3aeb0f167617b109dff5cd3
Roy C <61641010+canacosmic@users.noreply.github.com>
2020-08-29 08:45:00 -0500

    Here we go againg
    Starting over from scratch.

---
+++ au.Calc_v1/au.Calc_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  au.Calc_v1
+//
+//  Created by Rodrigo Canalizo Macías on 29/08/20.
+//  Copyright © 2020 Rodrigo Canalizo Macías. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
